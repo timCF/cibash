1d61473dade099c830fb80e85735eb86427b89d0
tim <tim@dis.lan>
2016-08-01 19:03:12 +0300

    add mysql 5.6

--- mac.sh
+++ mac.sh
@@ -4,7 +4,8 @@ brew update
 brew cask
 brew cask update
 brew cask install iterm2 google-chrome firefox atom amethyst slack skype java libreoffice
-brew install wget curl openssl elixir node gcc python nginx ruby protobuf
+brew install wget curl elixir node gcc python openssl nginx ruby protobuf
+brew install homebrew/versions/mysql56
 mix local.hex --force
 mix local.rebar --force
 apm install language-elixir autocomplete-elixir
