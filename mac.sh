b6af0337467b59e741ec52ca07e6eb1845c7d024
tim <tim@iMac-tim.local>
2016-09-02 16:55:53 +0300

    add iced

--- mac.sh
+++ mac.sh
@@ -11,7 +11,7 @@ brew tap homebrew/dupes
 brew install grep
 mix local.hex --force
 mix local.rebar --force
-apm install language-elixir autocomplete-elixir language-protobuf atom-jade language-sass language-erlang
+apm install language-elixir autocomplete-elixir language-protobuf atom-jade language-sass language-erlang language-iced-coffee-script
 npm cache clean
 npm install npm -g
 npm install phantomjs-prebuilt iced-coffee-script brunch bower -g
